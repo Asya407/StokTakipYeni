64dbb8dfe4f88add5642124df02329017c5d83d6
Asya değer <221903036@alparslan.edu.tr>
2025-08-27 22:40:55 +0300

    İlk commit

---
+++ GirisForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace StokTakip
+{
+    public partial class GirisForm : Form
+    {
+        public GirisForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=DESKTOP-PS08KAK\\SQLEXPRESS;Initial Catalog=StokDB;Integrated Security=True;";
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "SELECT COUNT(*) FROM Kullanicilar WHERE KullaniciAdi=@kadi AND Sifre=@sifre";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@kadi", txtKullaniciAdi.Text);
+                cmd.Parameters.AddWithValue("@sifre", txtSifre.Text);
+
+                con.Open();
+                int sonuc = (int)cmd.ExecuteScalar();
+                con.Close();
+
+                if (sonuc > 0)
+                {
+                    MessageBox.Show("Giriş başarılı!");
+                    this.Hide();
+                    Form1 anaForm = new Form1();
+                    anaForm.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Kullanıcı adı veya şifre yanlış.");
+                }
+            }
+        }
+    }
+}
